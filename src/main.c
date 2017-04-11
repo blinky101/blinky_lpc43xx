660c244a5b18256cd76b682f45e0948f67624f17
S de Wit <dev@sjorsdewit.name>
2017-04-11 12:40:57 +0200

    Interrupt-based version of blinky
    This version uses the systick timer interrupt instead of a blocking loop.

--- src/main.c
+++ src/main.c
@@ -1,19 +1,33 @@
 #include <chip.h>
 #include "board.h"
 
+// startup code needs this
 unsigned int stack_value = 0xA5A55A5A;
+
+// chip library depends on this
+const uint32_t OscRateIn = 12000000;
+const uint32_t ExtRateIn = 0;
+
+// LED blins at half this frequency
+#define SYSTICK_RATE_HZ (10)
+
+volatile bool led_state = false;
+
+void SysTick_Handler(void)
+{
+    Chip_GPIO_SetPinToggle(LPC_GPIO_PORT, 0, 13);
+}
 
 int main(void) {
     board_setup_pins();
     Chip_GPIO_SetPinState(LPC_GPIO_PORT, 0, 2, true);
     fpuInit();
 
-    while(1){
-        for(volatile int n=0;n<10000000;n++){} 
-        Chip_GPIO_SetPinState(LPC_GPIO_PORT, 0, 13, true);
-        for(volatile int n=0;n<10000000;n++){} 
-        Chip_GPIO_SetPinState(LPC_GPIO_PORT, 0, 13, false);
-    }
+	SystemCoreClockUpdate();
+	SysTick_Config(SystemCoreClock/SYSTICK_RATE_HZ);
+
+    while(1);
+
     return 0;
 }
 
